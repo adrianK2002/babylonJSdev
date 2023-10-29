8fb4b8ac356343d19d5feaa301aadd8aec6b312f
adrianK2002 <kurowskiadrian9@gmail.com>
2023-10-29 18:08:21 +0000

    element 1 4/5 are working
    still need to figure out shadows

--- babylonProj/element01/src/createStartScene.ts
+++ babylonProj/element01/src/createStartScene.ts
@@ -17,38 +17,55 @@ import {
     Texture,
     SpotLight,
     Color3,
+    DirectionalLight,
+    ShadowGenerator,
+    GroundMesh,
+    Animation, 
 } from "@babylonjs/core"
 //-----------------------------------------------------
 
 
 //--------------------------------------
 //MIDDLE OF CODE - FUNCTIONS
+function createGround(scene: Scene) {
+  const mat = new StandardMaterial("mat");
+  const color = new Color3(0.22, 0.03, 0.89);
+  mat.diffuseColor = color;
+  let ground = MeshBuilder.CreateGround(
+    "ground",
+    { width: 30, height: 25 },
+    scene,
+  );
+  ground.material = mat;
+  ground.receiveShadows = true;
+  return ground;
+}
   function createBox(scene: Scene, px: number, py:number, pz:number, sx: number, sy: number, sz: number) {
+    const mat = new StandardMaterial("mat");
+    const texture = new Texture("https://www.babylonjs-playground.com/textures/sphereMap.png");
+    mat.diffuseTexture = texture;
     let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
     box.position = new Vector3(px, py, pz);
     box.scaling = new Vector3(sx, sy, sz);
     //box.position.y = 3;
+    box.material = mat;
     return box;
   }
 
   //faced box function
   function createFacedBox(scene: Scene, px: number, py: number, pz: number) {
     const mat = new StandardMaterial("mat");
-    const texture = new Texture("https://assets.babylonjs.com/environments/numbers.jpg")
+    const texture = new Texture("https://www.babylonjs-playground.com/textures/bloc.jpg");
     mat.diffuseTexture = texture;
+    const faceUV = [];
+    faceUV[0] = new Vector4(0.0, 0.0, 0.5, 1.0); //rear face
+    faceUV[1] = new Vector4(0.25, 0, 0.5, 1.0); //front face
+    faceUV[2] = new Vector4(0.5, 0.0, 0.5, 1.0); //right side
+    faceUV[3] = new Vector4(0.75, 0, 0.5, 1.0); //left side
 
-    const faceUV = new Array(6);
+    
 
-//    for (let i = 0; i < 6; i++) {
-//      faceUV[i] = new Vector4(i / columns, 0, (i + 1) / columns, 1 / rows);
-//  }
-
-    const options = {
-        faceUV: faceUV,
-        wrap: true
-    };
-
-    let box = MeshBuilder.CreateBox("tiledBox", options, scene);
+    let box = MeshBuilder.CreateBox("box", {faceUV: faceUV, wrap: true});
     box.material = mat;
     box.position = new Vector3(px, py, pz);
     return box;
@@ -56,36 +73,72 @@ import {
 
   
   function createLight(scene: Scene) {
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    const light = new DirectionalLight("dir01", new Vector3(-1, -2, -1), scene);
+    light.position = new Vector3(20, 40, 20);
     return light;
   }
 
+  function createTorus(scene: Scene, px: number, py: number, pz: number) {
+    const mat = new StandardMaterial("mat");
+    const texture = new Texture("https://www.babylonjs-playground.com/textures/leopard_fur.JPG");
+    mat.diffuseTexture = texture;
+    var torus = Mesh.CreateTorus("torus", 4, 2, 30, false);
+    torus.position = new Vector3(px, py, pz);
+    const frameRate = 10;
+    torus.material = mat;
+    const xSlide = new Animation("xSlide", "position.x", frameRate, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
+  const keyFrames = []; 
+
+    keyFrames.push({
+        frame: 0,
+        value: 2
+    });
+
+    keyFrames.push({
+        frame: frameRate,
+        value: -2
+    });
+
+    keyFrames.push({
+        frame: 2 * frameRate,
+        value: 2
+    });
+
+    xSlide.setKeys(keyFrames);
+
+    scene.beginDirectAnimation(torus, [xSlide], 0, 2 * frameRate, true);  
+  }
+
+//  function createShadow(scene: Scene) {
+ //    const shadow = new ShadowGenerator(1024, light);
+ //    shadow.getShadowMap().renderList.push(torus);
+ //    shadow.useBlurExponentialShadowMap = true;
+//    shadow.useKernelBlur = true;
+ //    shadow.blurKernel = 64;
+//   }
+  
   function createSpotLight(scene: Scene, px: number, py: number, pz: number) {
-    var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 10, scene);
-    light.diffuse = new Color3(0.74, 1, 0.03);
-	  light.specular = new Color3(0.1, 0.04, 0.96);
+    var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 120, scene);
+    light.diffuse = new Color3(1, 0, 0);
+	  light.specular = new Color3(0, 1, 0);
     return light;
   }
-  
+
+
   function createSphere(scene: Scene) {
+    const mat = new StandardMaterial("mat");
+    const texture = new Texture("https://www.babylonjs-playground.com/textures/lava/lavatile.jpg");
+    mat.diffuseTexture = texture;
     let sphere = MeshBuilder.CreateSphere(
       "sphere",
       { diameter: 2, segments: 32 },
       scene,
     );
     sphere.position.y = 1;
+    sphere.material = mat;
     return sphere;
   }
-  
-  function createGround(scene: Scene) {
-    let ground = MeshBuilder.CreateGround(
-      "ground",
-      { width: 6, height: 6 },
-      scene,
-    );
-    return ground;
-  }
+
   
   function createArcRotateCamera(scene: Scene) {
     let camAlpha = -Math.PI / 2,
@@ -114,6 +167,7 @@ import {
       scene: Scene;
       box?: Mesh;
       faceBox?: Mesh;
+      torus?: Mesh;
       light?: Light;
       spotlight?: SpotLight;
       sphere?: Mesh;
@@ -126,8 +180,9 @@ import {
   
     //createBox(scene, posX, posY, posZ, scalX, scalY, scalZ)
     that.box = createBox(that.scene, 2, 5, 3, 3, 2, 1);
-    that.faceBox = createFacedBox(that.scene, 6, 2, 8)
+    that.faceBox = createFacedBox(that.scene, 2, 2, 8)
     that.light = createLight(that.scene);
+    that.torus = createTorus(that.scene, -30, 3, 3);
     that.spotlight = createSpotLight(that.scene, 0, 6, 0);
     that.sphere = createSphere(that.scene);
     that.ground = createGround(that.scene);
